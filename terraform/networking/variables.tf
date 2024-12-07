48816f1b6d0298f1f056901b7c521c1b5c1f6475
frodriguezcalo <68216190+frodriguezcalo@users.noreply.github.com>
2024-12-06 23:47:37 -0300

    Update variables.tf

--- terraform/networking/variables.tf
+++ terraform/networking/variables.tf
@@ -1,18 +1,9 @@
-
-variable "public_subnet_cidr" {
-  type = map(list(string))
-  default = {
-    dev     = ["10.0.1.0/24", "10.0.2.0/24"]
-    staging = ["10.0.3.0/24", "10.0.4.0/24"]
-    prod    = ["10.0.5.0/24", "10.0.6.0/24"]
-  }
+variable "environment" {
+    type = string
 }
 
-variable "private_subnet_cidr" {
-  type = map(list(string))
-  default = {
-    dev     = ["10.0.7.0/24", "10.0.8.0/24"]
-    staging = ["10.0.9.0/24", "10.0.10.0/24"]
-    prod    = ["10.0.11.0/24", "10.0.12.0/24"]
-  }
+variable "public_subnet_cidr" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
 }
