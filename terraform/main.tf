eea4a1ebff46b0ae0a9a61bef9e893015ff4f7f2
frodriguezcalo <68216190+frodriguezcalo@users.noreply.github.com>
2024-12-06 23:36:05 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,12 @@
 module "API_gateway" {
     source = "./api_gateway"
+    environment = var.environment
     alb_dns_name = aws_lb.main.dns_name
 }
 
 module "ECS" {
     source = "./ECS"
+    environment = var.environment
     subnets = module.networking.public_subnet_ids
     security_groups = [module.networking.ecs_security_group_id]
     tg_payments = module.networking.payments_target_group_arn
@@ -17,12 +19,20 @@ module "ECS" {
 
 module "networking" {
     source = "./networking"
+    environment = var.environment 
 }
 
 
 module "S3" {
     source = "./S3"
+    environment = var.environment
 }
+
+
+
+
+
+
 
 
 
